f0743cb2d0d993e2ae75cd2139cb014b0994b769
uguryagmur <ugur.gurkan.yagmur@gmail.com>
2023-09-13 19:00:03 +0300

    Bugs fixed.

---
+++ post-service/src/main/java/com/HRMS/mapper/IPostMapper.java
@@ -0,0 +1,17 @@
+package com.HRMS.mapper;
+
+import com.HRMS.dto.request.AddPostRequestDto;
+import com.HRMS.dto.response.AddPostResponseDto;
+import com.HRMS.repository.entity.Post;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE,componentModel = "spring")
+
+public interface IPostMapper {
+    IPostMapper INSTANCE = Mappers.getMapper(IPostMapper.class);
+
+    Post toPostFromDto(final AddPostRequestDto dto);
+    AddPostResponseDto toDtoFromPost(final Post post);
+}
--- post-service/src/main/java/com/HRMS/service/PostService.java
+++ post-service/src/main/java/com/HRMS/service/PostService.java
@@ -4,6 +4,7 @@ import com.HRMS.dto.request.AddPostRequestDto;
 import com.HRMS.dto.response.AddPostResponseDto;
 import com.HRMS.exceptions.PostException;
 import com.HRMS.exceptions.ErrorType;
+import com.HRMS.mapper.IPostMapper;
 import com.HRMS.repository.IPostRepository;
 import com.HRMS.repository.entity.Post;
 import com.HRMS.repository.enums.EStatus;
@@ -11,28 +12,35 @@ import com.HRMS.utils.ServiceManager;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 
 public class PostService extends ServiceManager<Post,String> {
     private final IPostRepository repository;
+    private final IPostMapper mapper;
 
 
-    public PostService(IPostRepository repository) {
+    public PostService(IPostRepository repository,IPostMapper mapper) {
         super(repository);
         this.repository = repository;
+        this.mapper=mapper;
     }
 
 
 
-        public AddPostResponseDto addPost(AddPostRequestDto requestDto) {
-            List<Post> postExists = repository.findByPostSubjectAndPostContent(requestDto.getPostSubject(), requestDto.getPostContent());
+        public Boolean addPost(AddPostRequestDto requestDto) {
+            Optional<Post> postExists = repository.findByPostSubjectAndPostContent(requestDto.getPostSubject(), requestDto.getPostContent());
 
-            if (!postExists.isEmpty()) {
+            if (postExists.isPresent()) {
 
                 throw new PostException(ErrorType.POST_ALREADY_EXISTS);
             }
+            Post post = mapper.toPostFromDto(requestDto);
+            post.setStatus(EStatus.PENDING);
+            save(post);
 
+/**
             Post post = Post.builder()
                     .companyName(requestDto.getCompanyName())
                     .companyId(requestDto.getCompanyId())
@@ -41,10 +49,12 @@ public class PostService extends ServiceManager<Post,String> {
                     .employeeId(requestDto.getEmployeeId())
                     .employeeName(requestDto.getEmployeeName())
                     .status(EStatus.PENDING)
-                    .state().build();
+                    .build();
+            save(post);
+            **/
 
-            post = repository.save(post);
-
+            AddPostResponseDto  responseDto = mapper.toDtoFromPost(post);
+/**
             AddPostResponseDto responseDto = AddPostResponseDto.builder()
                     .id(post.getId())
                     .companyName(post.getCompanyName())
@@ -55,8 +65,8 @@ public class PostService extends ServiceManager<Post,String> {
                     .employeeName(post.getEmployeeName())
                     .status(post.getStatus())
                     .build();
-
-            return responseDto;
+**/
+            return true;
         }
 }
 
