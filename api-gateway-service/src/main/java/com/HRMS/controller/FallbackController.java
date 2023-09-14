c29ec9b7aa40056c6579935e7776703dde649a2a
uguryagmur <ugur.gurkan.yagmur@gmail.com>
2023-09-14 23:24:52 +0300

    Url address changed.

--- api-gateway-service/src/main/java/com/HRMS/controller/FallbackController.java
+++ api-gateway-service/src/main/java/com/HRMS/controller/FallbackController.java
@@ -9,28 +9,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/fallback")
 public class FallbackController {
 
-    @GetMapping("/admin-service")
+    @GetMapping("/admin")
     public ResponseEntity<String> fallbackAdminService(){
         return ResponseEntity.ok("Admin servisi şu anda hizmet verememektedir. Lütfen daha sonra tekrar deneyiniz");
     }
-    @GetMapping("/auth-service")
+    @GetMapping("/auth")
     public ResponseEntity<String> fallbackAuthService(){
         return ResponseEntity.ok("Auth servisi şu anda hizmet verememektedir. Lütfen daha sonra tekrar deneyiniz");
     }
-    @GetMapping("/company-service")
+    @GetMapping("/company")
     public ResponseEntity<String> fallbackCompanyService(){
         return ResponseEntity.ok("Company servisi şu anda hizmet verememektedir. Lütfen daha sonra tekrar deneyiniz");
     }
-    @GetMapping("/employee-service")
+    @GetMapping("/employee")
     public ResponseEntity<String> fallbackEmployeeService(){
         return ResponseEntity.ok("Employee servisi şu anda hizmet verememektedir. Lütfen daha sonra tekrar deneyiniz");
     }
-    @GetMapping("/mail-service")
+    @GetMapping("/mail")
     public ResponseEntity<String> fallbackMailService(){
         return ResponseEntity.ok("Mail servisi şu anda hizmet verememektedir. Lütfen daha sonra tekrar deneyiniz");
     }
-    @GetMapping("/user-service")
+    @GetMapping("/user")
     public ResponseEntity<String> fallbackUserService(){
         return ResponseEntity.ok("User servisi şu anda hizmet verememektedir. Lütfen daha sonra tekrar deneyiniz");
+    }
+    @GetMapping("/post")
+    public ResponseEntity<String> fallbackPostService(){
+        return ResponseEntity.ok("Post servisi şu anda hizmet verememektedir. Lütfen daha sonra tekrar deneyiniz");
+    }
+    @GetMapping("/manager")
+    public ResponseEntity<String> fallbackManagerService(){
+        return ResponseEntity.ok("Manager servisi şu anda hizmet verememektedir. Lütfen daha sonra tekrar deneyiniz");
     }
 }
